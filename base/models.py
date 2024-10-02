66836e1595df40af9b328849ffc1710fee5d9655
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-02 09:53:27 +0500

    feat: create botuser and feedback model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class BotUser(models.Model):
+    user_id = models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
+    username = models.CharField(max_length=120, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Feedback(models.Model):
+    user_id = models.CharField(max_length=120, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    body = models.CharField(max_length=120, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
