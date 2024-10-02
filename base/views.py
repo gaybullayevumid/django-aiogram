8840b44e99f113ac458095f66bbd705d1b89c7f7
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-02 11:48:29 +0500

    feat: create views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import BotUser, Feedback
+from .serializers import BotUserSerializers, FeedbackSerialer
+from rest_framework.generics import ListCreateAPIView
 
 # Create your views here.
+
+class BotUserAPIView(ListCreateAPIView):
+    queryset = BotUser.objects.all()
+    serializer_class = BotUserSerializers
+
+
+class FeedbackAPIView(ListCreateAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerialer
